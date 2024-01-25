97744953ed65b966047ffac82f561805a0485f0e
IsrakAyasin <israkayasin@gmail.com>
2024-01-25 13:24:27 -0500

    update

--- p2.py
+++ p2.py
@@ -1,13 +1,15 @@
-def calculate(token):
-    list = token.split()
+def calculate(list):
     size = len(list) 
 
     result = 0
     i=0
-    while i<size:     
-        result+=int(list[i])
-        i+=1          
-    print(result)
+    if(size==0):
+        print("")
+    else:
+        while i<size:     
+            result+=int(list[i])
+            i+=1          
+        print(result)
 
 def parse(token):
     token = token.replace(" ", "")
@@ -15,20 +17,39 @@ def parse(token):
     token = token.replace("+-", "-")
     token = token.replace("++", "+")
     token = token.replace("-+", "-")
-    token = token.replace(" ", "")
+    token = token.replace(")", " ) ")
+    token = token.replace("(", "( ")
     
     token = token.replace("+"," ")
     token = token.replace("-"," -")
-    token = token.replace("="," = ")    
-    return token
+    token = token.replace("="," = ") 
+    list = token.split()   
+    return list
+
+def prenthesis(list):
+    prenNum = list.count("-(")
+
+    for i in range(prenNum):
+        start = list.index("-(")
+        end = list.index(")",start)
+        list.pop(start)
+        while start<end:
+            list.insert(start,"-")
+            start+=2
+        list.pop(end+1)
+
+    result_string = ' '.join(list)
+
+    return(result_string)
+
 
 try:
     while True:
         token=input()
         #pren
+        list = parse(token)
         
-
-        token = parse(token)
-        calculate(token)
+        print(parse(prenthesis(list)))
+        #calculate(list)
 except EOFError:
         print("")
